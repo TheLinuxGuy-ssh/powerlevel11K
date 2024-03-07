6c77c0db3a8bc39751676b1165f683b80ad2efca
rohansharma-developer <rohansharma.developer@gmail.com>
2024-03-07 16:44:24 +0530

    changes

--- p10k.sh
+++ p10k.sh
@@ -10,22 +10,26 @@ elif command -v brew &> /dev/null; then
 else
     echo "No supported package manager (apt or pacman) found on your system."
 fi
+
 update_apt() {
 sudo apt update
 sudo apt upgrade
 sudo apt install zsh
 }
+
 update_pacman() {
 sudo pacman -Syu
 sudo pacman -S zsh
 sudo pacman -S zsh-syntax-highlighting
 }
+
 update_brew() {
     brew update;
     brew upgrade;
     echo "[+] Installing ZSH..."
     brew install zsh;
 }
+
 echo "[!] Configuring ZSH."
 cp -r ./.* ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
